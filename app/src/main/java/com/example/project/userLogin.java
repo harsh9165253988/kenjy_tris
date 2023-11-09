31d4cc1d4d353542e9a37d120138ee306dba17a4
faruk mansuri <farukhmansuri11122@gmail.com>
2023-11-09 10:01:11 -0800

    sigup and login for user are authenticate through firebase

--- app/src/main/java/com/example/project/userLogin.java
+++ app/src/main/java/com/example/project/userLogin.java
@@ -1,50 +1,86 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class userLogin extends AppCompatActivity {
 
-    Button requestButton;
-    TextView alreadySignup;
+    private Button requestButton;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private TextView alreadySignup;
     private AwesomeValidation awesomeValidation;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 
-        requestButton = (Button) findViewById(R.id.buttonLogin);
-        alreadySignup = (TextView) findViewById(R.id.new_member);
-        requestButton.setOnClickListener(mMyListener);
-        alreadySignup.setOnClickListener(mMyListener);
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editPassword);
+        requestButton = findViewById(R.id.buttonLogin);
+        alreadySignup = findViewById(R.id.new_member);
+
+        // Validation rules
         String regexPassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{8,}";
         awesomeValidation.addValidation(this, R.id.editTextEmail, Patterns.EMAIL_ADDRESS, R.string.emailerror);
         awesomeValidation.addValidation(this, R.id.editPassword, regexPassword, R.string.passworderror);
-    }
-    private View.OnClickListener mMyListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            if (v.getId() == R.id.buttonLogin) {
+
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 if (awesomeValidation.validate()) {
-                    Toast.makeText(getApplicationContext(), "It's Working!", Toast.LENGTH_LONG).show();
+                    loginUser();
                 }
-            } else if (v.getId() == R.id.new_member) {
+            }
+        });
+
+        alreadySignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), userSignup.class);
                 startActivity(i);
             }
-        }
-
-    };
+        });
     }
+
+    private void loginUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign-in successful
+                            Toast.makeText(getApplicationContext(), "Sign-in successful", Toast.LENGTH_SHORT).show();
+                            // You can navigate to another activity upon successful sign-in if needed.
+                        } else {
+                            // Sign-in failed
+                            Toast.makeText(getApplicationContext(), "Sign-in failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/project/userSignup.java
+++ app/src/main/java/com/example/project/userSignup.java
@@ -1,70 +1,103 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import com.airbnb.lottie.LottieAnimationView;
-import android.os.Bundle;
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.basgeekball.awesomevalidation.utility.custom.SimpleCustomValidation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class userSignup extends AppCompatActivity {
-    Button requestButton;
-    TextView alreadySignin;
+
+    private Button requestButton;
+    private TextView alreadySignin;
+    private EditText emailEditText;
+    private EditText passwordEditText;
     private AwesomeValidation awesomeValidation;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_signup);
-        LottieAnimationView lottieBackground = findViewById(R.id.lottieBackground);
-        lottieBackground.playAnimation();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        final EditText fullname = findViewById(R.id.editTextName);
+        emailEditText = findViewById(R.id.editTextEmail);
+        final EditText number = findViewById(R.id.editTextMobile);
+        final EditText age = findViewById(R.id.editTextAge);
+        passwordEditText = findViewById(R.id.editPassword);
+
+        requestButton = findViewById(R.id.buttonSign);
+        alreadySignin = findViewById(R.id.already_signin);
+
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 
-        requestButton = (Button) findViewById(R.id.buttonSign);
-        alreadySignin = (TextView) findViewById(R.id.already_signin);
-        requestButton.setOnClickListener(mMyListener);
-        alreadySignin.setOnClickListener(mMyListener);
-
         String regexPassword = "(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#\\$%\\^&\\*\\(\\)\\-_\\+=\\{\\}\\[\\]\\|\\;:\"<>,./\\?]).{8,}";
-        //adding validation to edittexts
+
         awesomeValidation.addValidation(this, R.id.editTextName, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
         awesomeValidation.addValidation(this, R.id.editTextEmail, Patterns.EMAIL_ADDRESS, R.string.emailerror);
         awesomeValidation.addValidation(this, R.id.editTextMobile, "^[2-9]{2}[0-9]{8}$", R.string.mobileerror);
         awesomeValidation.addValidation(this, R.id.editPassword, regexPassword, R.string.passworderror);
-        // Adding custom validation for age
+
         awesomeValidation.addValidation(this, R.id.editTextAge, new SimpleCustomValidation() {
             @Override
             public boolean compare(String input) {
-                // Check if age is 18 or older
                 try {
-                    int age = Integer.parseInt(input);
-                    return age >= 14;
+                    int userAge = Integer.parseInt(input);
+                    return userAge >= 14;
                 } catch (NumberFormatException e) {
                     return false;
                 }
             }
         }, R.string.ageerror);
-    }
 
-
-    private View.OnClickListener mMyListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            if (v.getId() == R.id.buttonSign) {
+        requestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 if (awesomeValidation.validate()) {
-                    Toast.makeText(getApplicationContext(), "It's Working!", Toast.LENGTH_LONG).show();
+                    registerUser();
                 }
-            } else if (v.getId() == R.id.already_signin) {
+            }
+        });
+
+        alreadySignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), userLogin.class);
                 startActivity(i);
             }
-        }
-
-    };
+        });
     }
 
+    private void registerUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(userSignup.this, "Signup successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(userSignup.this, "Signup failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
